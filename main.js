fcb42a1110d8b26d1dd58293b2f379882d5097db
salvoparisi <psalvo065@gmail.com>
2024-10-17 15:07:24 +0200

    add loading feature

--- main.js
+++ main.js
@@ -2,15 +2,26 @@ const generate = document.getElementById('generate');
 const emailList = document.getElementById('email-list');
 
 generate.addEventListener('click', function () {
+    generate.innerHTML=`
+        <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+        </div>
+    `
+    emailNum=0;
     emailList.innerHTML = '';
     for (let i = 0; i < 10; i++) {
         fetch('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(response => response.json())
         .then(data => {
             emailList.innerHTML += `<li class="list-group-item">${data.response}</li>`;
+            emailNum++;
+            if(emailNum==10){
+                generate.innerHTML='Generate'
+            }
         })
         .catch(error => {
             console.error('Errore nel recupero dell\'email:', error);
         });
     }
+
 });
