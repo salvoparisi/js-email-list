324379dd874374ab6e5103a8708f360b0bcbcbad
salvoparisi <psalvo065@gmail.com>
2024-10-17 14:57:26 +0200

    add js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-const generate = document.getElementById('generate')
-const emailList = document.getElementById('email-list')
+const generate = document.getElementById('generate');
+const emailList = document.getElementById('email-list');
 
+generate.addEventListener('click', function () {
+    emailList.innerHTML = '';
+    for (let i = 0; i < 10; i++) {
+        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => response.json())
+        .then(data => {
+            emailList.innerHTML += `<li class="list-group-item">${data.response}</li>`;
+        })
+        .catch(error => {
+            console.error('Errore nel recupero dell\'email:', error);
+        });
+    }
+});
